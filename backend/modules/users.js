257a7d3f1a0e6618bf89e41e8f140c34c3c5c19d
Acsbeni <Acsbenjamin@turr.hu>
2025-11-01 21:14:38 +0100

    User backend elkészítése

--- backend/modules/users.js
+++ backend/modules/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const {query} = require('../utils/database');
 
+//userek lehívása
 router.get("/", (req, res)=>{
     
     query(`SELECT * FROM users` ,[], (error, results) =>{
@@ -10,5 +11,81 @@ router.get("/", (req, res)=>{
         res.status(200).json(results)
     },req);
 })
+
+//user lehívása id-vel
+router.get("/:id", (req, res)=>{
+    let id = req.params.id;
+    query(`SELECT * FROM users WHERE id=?` ,[id], (error, results) =>{
+        if(error) return res.status(500).json({errno: error.errno, msg: "Hiba történt :("}) ;
+      
+        res.status(200).json(results)
+    },req);
+})
+
+//új user insert
+router.post("/", (req, res)=>{
+    const { name, password, email, status, role} = req.body;
+    query(`INSERT INTO users(name, password, email, status, role) VALUES (?,?,?,?,?)` ,[ name, password, email, status, role], (error, results) =>{
+        if(error) return res.status(500).json({errno: error.errno, msg: "Hiba történt :("}) ;
+      
+        res.status(200).json(results)
+    },req);
+})
+//login user
+router.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  query(
+    `SELECT * FROM users WHERE email = ? AND password = ?`,[email, password],(error, results) => {
+      if (error) return res.status(500).json({ errno: error.errno, msg: "Hiba történt :(" });
+      if (results.length === 0) {
+        return res.status(401).json({ msg: "Hibás email vagy jelszó!" });
+      }
+      res.status(200).json(results[0]); 
+    },
+    req);
+})
+
+
+//user update id alapján
+router.patch("/:id", (req, res)=>{
+    let ID = req.params.id 
+    const { name, password, email, status, role} = req.body;
+    query(`UPDATE users SET name=?,password=?,email=?,status=?,role=? WHERE ID =?` ,[ name, password, email, status, role, ID], (error, results) =>{
+        if(error) return res.status(500).json({errno: error.errno, msg: "Hiba történt :("}) ;
+      
+        res.status(200).json(results)
+    },req);
+})
+//user jelszó frissítése id alapján
+router.patch("/password/:id", (req, res)=>{
+    let ID = req.params.id 
+    const {password} = req.body;
+    query(`UPDATE users SET password=? WHERE ID =?` ,[  password, ID], (error, results) =>{
+        if(error) return res.status(500).json({errno: error.errno, msg: "Hiba történt :("}) ;
+      
+        res.status(200).json(results)
+    },req);
+})
+//user név,email frissítése id alapján
+router.patch("/profile/:id", (req, res)=>{
+    let ID = req.params.id 
+    const { name,email} = req.body;
+    query(`UPDATE users SET name=?,email=? WHERE ID =?` ,[ name, email,ID], (error, results) =>{
+        if(error) return res.status(500).json({errno: error.errno, msg: "Hiba történt :("}) ;
+      
+        res.status(200).json(results)
+    },req);
+})
+
+//user törlése id alapján
+router.delete("/:id", (req, res)=>{
+    let id = req.params.id;
+    query(`DELETE FROM users WHERE id=?` ,[id], (error, results) =>{
+        if(error) return res.status(500).json({errno: error.errno, msg: "Hiba történt :("}) ;
+      
+        res.status(200).json(results)
+    },req);
+})
+
 
 module.exports = router;
